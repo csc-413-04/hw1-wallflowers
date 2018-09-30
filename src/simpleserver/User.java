7de0210a8723c35e083e0afdc0c6deb789b53e3d
David Dropping <ddroppin@mail.sfsu.edu>
2018-09-30 11:13:48 -0700

    user getter

--- src/simpleserver/User.java
+++ src/simpleserver/User.java
@@ -1,4 +1,20 @@
 package simpleserver;
 
 public class User {
+
+    private int userID;
+    private String userName;
+
+    public User(int userID, String userName){
+        this.userID = userID;
+        this.userName = userName;
+    }
+
+    public int getUserId(){
+        return userID;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
 }
