188a0c239a41a85e8fbcf428548aac2ec0ba4d30
David Dropping <ddroppin@mail.sfsu.edu>
2018-09-29 23:38:37 -0700

    gson

---
+++ src/simpleserver/Database.java
@@ -0,0 +1,12 @@
+package simpleserver;
+
+import java.util.HashMap;
+
+public class Database {
+
+    //private HashMap<String, User> userHashMap = new HashMap<>();
+    //private HashMap<String, Posts> postsHashMap = new HashMap<>();
+
+
+
+}
